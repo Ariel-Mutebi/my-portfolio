3319f018bfa571c4778299ecbb7a2411b9e36d6e
black-RAM <pplanes311@gmail.com>
2026-02-15 09:19:06 +0300

    better sizing of reception

--- src/components/ThreeFourAspectRatio.tsx
+++
@@ -1,4 +0,0 @@
-import type { FC, PropsWithChildren } from "react";
-
-export const ThreeFourAspectRatio: FC<PropsWithChildren> = ({ children }) =>
-  <div className="aspect-3/4 overflow-hidden">{children}</div>
--- src/spaces/reception/Reception.tsx
+++ src/spaces/reception/Reception.tsx
@@ -1,20 +1,21 @@
 import officeFlex from "./images/office-flex.jpg";
 import officeSelfie from "./images/office-selfie.jpg";
 import napoleonPainting from "./images/napoleon-in-his-study.jpeg";
-import { ThreeFourAspectRatio } from "../../components/ThreeFourAspectRatio.tsx";
 
 export default function Reception() {
   return (
-    <section className="grid grid-cols-3 relative">
-      <img src={officeFlex} alt="boy in office doing front double biceps" />
-      <header className="absolute">
-        <h1>Ariel Robert Mutebi</h1>
-        <h2>At AR Mutebi LLC</h2>
-      </header>
-      <img src={officeSelfie} alt="selfie of a boy in a dressing shirt and vest" />
-      <ThreeFourAspectRatio>
-        <img src={napoleonPainting} alt="Painting: Emperor Napoleon in his study" />
-      </ThreeFourAspectRatio>
-    </section>
+    <div className="h-dvh max-h-dvh flex items-center justify-center">
+      <section className="grid grid-cols-3 relative gap-4 m-8">
+        <img src={officeFlex} className="max-h-[80dvh]" alt="boy in office doing front double biceps" />
+        <header className="absolute">
+          <h1>Ariel Robert Mutebi</h1>
+          <h2>At AR Mutebi LLC</h2>
+        </header>
+        <img src={officeSelfie} className="max-h-[80dvh]" alt="selfie of a boy in a dressing shirt and vest" />
+        <div className="aspect-3/4 overflow-hidden max-h-[80dvh]">
+          <img src={napoleonPainting} alt="Oil painting: Emperor Napoleon in his study" />
+        </div>
+      </section>
+    </div>
   );
 }
