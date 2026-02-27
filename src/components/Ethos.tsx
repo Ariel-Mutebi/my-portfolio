eedda13fb5a7658f92f849b725d73a4e0598ede3
black-RAM <pplanes311@gmail.com>
2026-02-27 20:07:33 +0300

    The Ariel Ethos header

--- src/components/Ethos.tsx
+++ src/components/Ethos.tsx
@@ -1,8 +1,12 @@
+import "./Ethos.css";
+
 export function Ethos() {
   return (
-    <section id="ethos" className="h-dvh">
-      <div className="bg-lime-200">
-        <h2 className="playfair-display text-8xl">The Ariel <span className="italic">Ethos</span></h2>
+    <section id="ethos">
+      <div className="bg-lime-200 min-h-dvh">
+        <h2 className="playfair-display text-8xl text-zinc-800 pt-8 pr-24 text-right">
+          The Ariel <span className="italic ethos-gradient tracking-wide">Ethos</span>
+        </h2>
       </div>
     </section>
   );
