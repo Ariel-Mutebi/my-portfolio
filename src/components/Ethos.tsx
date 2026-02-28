c715f76957db4e3e8669a8715973554d21af65b5
black-RAM <pplanes311@gmail.com>
2026-02-28 14:02:02 +0300

    centered main word in first ethos article

--- src/components/Ethos.tsx
+++ src/components/Ethos.tsx
@@ -4,16 +4,19 @@ import "./Ethos.css";
 export function Ethos() {
   return (
     <section id="ethos">
-      <article className="bg-lime-200 min-h-dvh">
-        <h2 className="playfair-display text-8xl text-zinc-800 pt-8 pr-24 text-right">
+      <article className="bg-lime-200 min-h-dvh relative">
+        <h2 className="playfair-display text-8xl text-zinc-800 pt-8 pr-24 text-right z-10">
           The Ariel <span className="italic ethos-gradient tracking-wide">Ethos</span>
         </h2>
         <TextReveal
           text="Creating lasting first impressions of"
-          className="ml-[20dvw] mt-8 text-4xl montserrat"
+          className="ml-[20dvw] mt-8 text-4xl montserrat z-10"
           startColor="#ecfccb"
           endColor="#52525b"
         />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <p className="montserrat text-6xl mt-8 font-bold text-center">confidence</p>
+        </div>
       </article>
     </section>
   );
